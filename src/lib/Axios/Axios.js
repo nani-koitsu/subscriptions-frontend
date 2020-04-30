393a336208e5a8c3ae62a1585bc3559237bebb02
nani-koitsu <itadaki.onegai@gmail.com>
2020-04-30 09:42:01 -0400

    changed axios base

--- src/lib/Axios/Axios.js
+++ src/lib/Axios/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const Axios = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api' : '',
+  baseURL: process.env.NODE_ENV === 'development' ? '/api' : '',
   timeout: 50000,
   crossdomain: true,
   headers: {
